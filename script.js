823de35ed853aff4533f07c207bec83b36f4b791
Hector Sanchez <monkhectorsanchez@gmail.com>
2021-07-24 19:53:23 -0400

    added toggle box selection

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+const container = document.querySelector('.container');
 const docs = document.getElementById('docs');
 const close = document.getElementById('close');
 const on = document.getElementById('on');
+const box = document.querySelectorAll('.box');
+const selectedBoxes = document.querySelectorAll('.box.selected');
 
 // Docs navigation
 docs.addEventListener('click', () =>
   document.body.classList.toggle('show-docs')
 );
+
+// Box selection
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('box')) {
+    e.target.classList.toggle('selected');
+  }
+})

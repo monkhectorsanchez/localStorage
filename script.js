2e7ea3dc5ac36bd2bf8e5758d9a17e6adc0eff76
Hector Sanchez <monkhectorsanchez@gmail.com>
2021-07-24 20:43:11 -0400

    implemented local storage

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ const docs = document.getElementById('docs');
 const close = document.getElementById('close');
 const on = document.getElementById('on');
 const box = document.querySelectorAll('.box');
-const selectedBoxes = document.querySelectorAll('.box.selected');
 
 // Docs navigation
 docs.addEventListener('click', () =>
   document.body.classList.toggle('show-docs')
 );
 
+// Total boxes selected
+function boxesSelected() {
+  const selectedBoxes = document.querySelectorAll('.box.selected');
+
+  const boxIndex = [...selectedBoxes].map((b) => [...box].indexOf(b));
+
+  localStorage.setItem('selectedBoxes', JSON.stringify(boxIndex));
+}
+
 // Box selection
 container.addEventListener('click', (e) => {
   if (e.target.classList.contains('box')) {
     e.target.classList.toggle('selected');
   }
+
+  boxesSelected();
 })
+
